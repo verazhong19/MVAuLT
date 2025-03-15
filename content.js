a0f67ddcb1610fa316462984777e8eeaa70cbf50
perfect ace <ace@isabel.local>
2025-03-15 14:19:09 -0700

    added timestamp

--- content.js
+++ content.js
@@ -3,13 +3,16 @@ document.addEventListener("keydown", async (event) => {
       event.preventDefault();
       const video = document.querySelector("video");
       if (video && video.paused) {
+        const player = document.getElementsByClassName('video-stream')[0];
+        const timestamp = player.currentTime;
+        console.log(timestamp);
         const canvas = document.createElement("canvas");
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         const ctx = canvas.getContext("2d");
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         const imageData = canvas.toDataURL("image/png");
-        chrome.storage.local.set({ [Date.now()]: { image: imageData, caption: "" } }, () => {
+        chrome.storage.local.set({ [Date.now()]: { image: imageData, timestamp: timestamp, caption: "" } }, () => {
           console.log("Frame saved!");
         });
       }
