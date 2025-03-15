c4dcb3866e0516cc629333fc761403fd03febeb9
Vera Zhong <42988550+verazhong19@users.noreply.github.com>
2025-03-15 15:52:30 -0700

    convert timestamp to hours minutes seconds

--- content.js
+++ content.js
@@ -4,7 +4,16 @@ document.addEventListener("keydown", async (event) => {
       const video = document.querySelector("video");
       if (video && video.paused) {
         const player = document.getElementsByClassName('video-stream')[0];
-        const timestamp = player.currentTime;
+        // const timestamp = player.currentTime;
+        var hours = parseInt(player.currentTime / (60 * 60), 10);
+        var minutes = parseInt((player.currentTime / 60) % 60, 10); // Ensure minutes resets at 60
+        var seconds = player.currentTime % 60;
+        
+        var time = hours > 0 
+          ? hours + ":" + String(minutes).padStart(2, "0") + ":" + String(seconds.toFixed(0)).padStart(2, "0")
+          : minutes + ":" + String(seconds.toFixed(0)).padStart(2, "0");
+        
+        const timestamp = time;
         const canvas = document.createElement("canvas");
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
